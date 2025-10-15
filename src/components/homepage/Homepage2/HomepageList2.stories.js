e6cb929c7160d8d558391e7b263a0cb2120fd43f
zengyuwen <@3520753196@qq.com>
2025-10-15 22:06:05 +0800

    序号

---
+++ src/components/homepage/Homepage2/HomepageList2.stories.js
@@ -0,0 +1,16 @@
+import HomepageList2 from './HomepageList2.vue';
+
+export default {
+
+  title: 'components/homepage/Homepage2/HomepageList2',
+
+  component: HomepageList2,
+
+  tags: ['autodocs'],
+};
+
+export const Default = {
+  args: {
+
+  },
+};
